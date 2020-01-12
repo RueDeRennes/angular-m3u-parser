505f64e2b27ffda7feb4ff6406ca0eed2b230972
RueDeRennes <realsancezz@gmail.com>
2020-01-12 09:14:34 +0100

    6 files modified, 2 added, 1 deleted

--- src/app/Views/data-table-view/data-table-view.component.ts
+++ src/app/Views/data-table-view/data-table-view.component.ts
@@ -16,6 +16,9 @@ import {
 } from "@angular/cdk/drag-drop";
 import { MatTable } from "@angular/material/table";
 import { M3UEntry } from "../../Models/Models";
+import { MatSort } from "@angular/material/sort";
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: "app-data-table-view",
@@ -23,23 +26,27 @@ import { M3UEntry } from "../../Models/Models";
   styleUrls: ["./data-table-view.component.css"]
 })
 export class DataTableViewComponent {
-  private _dataSource: Array<any>;
-
   @ViewChild("table", { static: true })
   table: MatTable<M3UEntry>;
-  displayedColumns: string[] = ["title", "src", "attributes"];
 
-  @Input()
-  public get dataSource(): Array<any> {
-    return this._dataSource;
-  }
-  public set dataSource(value: Array<any>) {
-    this._dataSource = value;
+  @ViewChild(MatSort, { static: true })
+  sort: MatSort;
+
+  dataSourceColumns: string[] = ["title", "src", "attributes"];
+
+  dataSource: MatTableDataSource<any>;
+
+  ngOnInit(): void {
+    this.dataSource.sort = this.sort;
   }
 
-  dropTable(event: CdkDragDrop<Array<M3UEntry>>) {
-    const prevIndex = this.dataSource.findIndex(d => d === event.item.data);
-    moveItemInArray(this.dataSource, prevIndex, event.currentIndex);
-    this.table.renderRows();
+  setDataSource(data: Array<any>): void {
+    this.dataSource = new MatTableDataSource(data);
   }
+
+  // dropTable(event: CdkDragDrop<Array<M3UEntry>>) {
+  //   const prevIndex = this.dataSource.findIndex(d => d === event.item.data);
+  //   moveItemInArray(this.dataSource, prevIndex, event.currentIndex);
+  //   this.table.renderRows();
+  // }
 }
