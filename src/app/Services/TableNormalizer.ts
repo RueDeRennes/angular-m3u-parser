9346f8f866c37e1b73855ae4700071b653a93fda
RueDeRennes <realsancezz@gmail.com>
2020-01-18 13:42:02 +0100

    3 files modified, 1 added

--- src/app/Services/TableNormalizer.ts
+++ src/app/Services/TableNormalizer.ts
@@ -1,14 +1,38 @@
+import { Unique } from "./../Models/Unique";
+
+class TableSchema {
+  private readonly _skeleton: Object;
+  private readonly _id: string;
+  constructor(id: string, skeleton: Object) {
+    this._id = id;
+    this._skeleton = skeleton;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get skeleton(): Object {
+    return this._skeleton;
+  }
+}
+
 export class TableNormalizer {
+  private readonly _cache: Array<TableSchema>;
+
   public normalize<T extends Object>(data: T | Array<T>): T | Array<T> {
+    // find a why to store the object schema.
+    const schema = new TableSchema(Unique.new(), this.createSchema(data));
+
     if (Array.isArray(data)) {
-       return data.map(x => this.toFlat(data));
-    }else {
+      return data.map(x => this.toFlat(data));
+    } else {
       return this.toFlat(data);
     }
   }
 
   public denormalize<T>(data: T): void {
-    throw new Error('NOT IMPLEMENTED');
+    throw new Error("NOT IMPLEMENTED");
   }
 
   private toFlat(self: object): any {
@@ -16,11 +40,19 @@ export class TableNormalizer {
       {},
       ...(function flatten(x: object): any {
         return [].concat(
-          ...Object.keys(x).map(k =>
-            typeof x[k] === "object" ? flatten(x[k]) : { [k]: x[k] }
-          )
+          ...Object.keys(x).map(k => {
+            // Convert trainCase to camelCase
+            k = k.replace(/-([a-z])/g, g => {
+              return g[1].toUpperCase();
+            });
+            return typeof x[k] === "object" ? flatten(x[k]) : { [k]: x[k] };
+          })
         );
       })(self)
     );
+  }
+
+  private createSchema<T>(data: T | Array<T>): Object {
+    return {};
   }
 }
